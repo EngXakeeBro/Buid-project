cc95bfbd5642ed3ac2bd783334343e574a4ee34c
EngXakeeBro <152479644+EngXakeeBro@users.noreply.github.com>
2023-12-04 08:57:13 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+function Cv(){
+  let marks=[50,23,45];
+  return(
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Zakir Ullah</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <h1>The grade sheet</h1>
+    <ol>
+      <li>The marks index is marks{marks[0 ]}</li>
+    </ol>
     </>
   )
 }
-
-export default App
